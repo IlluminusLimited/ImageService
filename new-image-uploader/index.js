8fb408962f0716e2990a0cbb92819951a82f414a
scytherswings <scytherswings@gmail.com>
2018-03-18 13:52:13 -0400

    use variables for bucket name

--- new-image-uploader/index.js
+++ new-image-uploader/index.js
@@ -7,5 +7,6 @@ const FileBuilder = require('../new-image-uploader/file-builder');
 const RequestHandler = require('../new-image-uploader/request-handler');
 
 module.exports.upload = (event, context, callback) => {
-    RequestHandler.perform(event, context, callback, new FileBuilder(), new FileWriter(new AWSS3()), 'test-image-service-new-bucket-test');
+    let bucket = process.env['BUCKET_NAME'] == null ? 'test-image-service-new-bucket-test' : process.env['BUCKET_NAME'];
+    RequestHandler.perform(event, context, callback, new FileBuilder(), new FileWriter(new AWSS3()),bucket );
 };
