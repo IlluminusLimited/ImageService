ef3739b243024cdf73e27f6234917ddd3ec04f5d
scytherswings <scytherswings@gmail.com>
2018-03-18 18:32:55 -0400

    use jpeg for images

--- thumbnail-processor/handler.js
+++ thumbnail-processor/handler.js
@@ -83,26 +83,28 @@ function callStepFunction(resizeParams) {
 }
 
 module.exports.generateThumbnail = (event, context, callback) => {
+    const format = 'jpeg';
     const key = event.Key;
     const bucket = event.Bucket;
     // const match = key.match(/((\d+)x(\d+))\/(.*)/);
     const width = parseInt(event.width, 10);
     const height = parseInt(event.height, 10);
-    let newKey = key.replace(/(\.[\w\d_-]+)$/i, '_' + width + 'x' + height + '$1');
+    let newKey = key.replace(/(\.[\w\d_-]+)$/i, '_' + width + 'x' + height + '.' + format);
     newKey = newKey.replace(/raw\//i, '');
 
     console.log("New key is: " + newKey)
 
     S3.getObject({Bucket: bucket, Key: key}).promise()
         .then(data => Sharp(data.Body)
-            .max(width, height)
-            .toFormat('png')
+            .resize(width, height)
+            .max()
+            .toFormat(format)
             .toBuffer()
         )
         .then(buffer => S3.putObject({
                 Body: buffer,
                 Bucket: bucket,
-                ContentType: 'image/png',
+                ContentType: 'image/'+ format,
                 Key: newKey,
             }).promise()
         )
