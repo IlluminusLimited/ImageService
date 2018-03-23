eed6cb8a3616f0fa6a0c55de2eae61262c7732b8
scytherswings <scytherswings@gmail.com>
2018-03-22 22:33:16 -0400

    fix what directory things are run from

--- etc/deploy-dev.sh
+++ etc/deploy-dev.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+cd ..
 ruby serverless.rb package dev false
 make all
 sls deploy --package .serverless
--- etc/deploy-prod.sh
+++ etc/deploy-prod.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+cd ..
 ruby serverless.rb package prod false
 make all
 sls deploy --package .serverless
