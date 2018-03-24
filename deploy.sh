2f0e3da83590b1a5d055d38f437c842dc48e84d9
scytherswings <scytherswings@gmail.com>
2018-03-24 19:39:40 -0400

    stupid shit

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ echo "Called with stage: $1"
 
 make all
 
-docker run --rm --volume ${PWD}/lambda:/build amazonlinux:nodejs sh build.sh $1
+docker run --volume ${PWD}/lambda:/build amazonlinux:nodejs npm install
+
+docker run --rm --volume ${PWD}/lambda:/build amazonlinux:nodejs ruby serverless.rb deploy $1 false
--- lambda/build.sh
+++ lambda/build.sh
@@ -4,6 +4,6 @@ set -ex
 
 echo "Called with stage: $1"
 
-echo $(npm install)
+npm install
 
-echo $(ruby serverless.rb deploy $1 false)
+ruby serverless.rb deploy $1 false
