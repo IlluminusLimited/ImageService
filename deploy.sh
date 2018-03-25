f992981103c06dd869dda7bfeadbb8b84b53ac2a
scytherswings <scytherswings@gmail.com>
2018-03-24 23:30:20 -0400

    install serverless

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -ex
 echo "Called with stage: $1"
 
 ruby serverless.rb package $1 false && \
- make all && \
- sls deploy --package .serverless
+make all && \
+serverless deploy --package .serverless
