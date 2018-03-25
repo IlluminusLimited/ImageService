3cae8b2936ae7c765e6fc19c1cb3c69a190e87dd
stretch1414 <jmoser1414@gmail.com>
2018-03-25 03:50:01 -0400

    Restructure the upload logic

--- lambda/image-uploader.js
+++ lambda/image-uploader.js
@@ -1,12 +1,100 @@
 'use strict';
 
-const AWSS3 = require('aws-sdk/clients/s3');
-
 const FileWriter = require('./file-writer');
 const FileBuilder = require('./file-builder');
-const RequestHandler = require('./request-handler');
+const AWSS3 = require('aws-sdk/clients/s3');
+const _ = require('lodash');
+
+module.exports = class ImageUploader {
+    constructor(fileBuilder, fileWriter) {
+        this.FileBuilder = _.isUndefined(fileBuilder) ? new FileBuilder() : fileBuilder;
+        this.FileWriter = _.isUndefined(fileWriter) ? new FileWriter(new AWSS3) : fileWriter;
+    }
+
+    static parseRequest(event, callback) {
+        let data = JSON.parse(event.body).data;
+
+        // TODO - JSON Schema Validation
+
+        let image = data["image"];
+        let metadata = data['metadata'];
+        let year = null;
+        let userId = null;
+        if (metadata != null) {
+            year = metadata['year'];
+            userId = metadata['user_id'];
+        }
+
+        if (data == null || userId == null || metadata == null || year == null) {
+            let response = {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: "Bad Request. Required fields are missing.",
+                    example_body: {
+                        data: {
+                            metadata: {
+                                user_id: "uuid",
+                                year: "integer year"
+                            },
+                            image: "base64 encoded image"
+                        }
+                    }
+                })
+            };
+            callback(response)
+        }
+        else {
+            callback(undefined, {
+                metadata: metadata,
+                image: image
+            });
+        }
+    }
+
+    static perform(event, context, callback) {
+        let tasks = [];
+        tasks.push((callback) => {
+            ImageUploader.parseRequest(event, callback);
+        });
+        tasks.push((parsedRequest, callback) => {
+            if (_.isObject(parsedRequest) &&
+                _.isNumber(parsedRequest.statusCode) &&
+                parsedRequest.statusCode === 400) {
+                callback("The parsed request contained an error");
+            }
+            else {
+                this.FileBuilder.getFile(parsedRequest.image, (err, imageFile) => {
+                    callback(err, parsedRequest, imageFile);
+                });
+            }
+        });
+        tasks.push((parsedRequest, imageFile, callback) => {
+            imageFile.Bucket = process.env.BUCKET_NAME;
+            imageFile.Metadata = parsedRequest.metadata;
+            this.FileWriter.saveObject(imageFile, (err, data) => {
+                callback(err, imageFile, data);
+            });
+        });
+        async.waterfall(tasks, (err, data) => {
+            if (err) {
+                callback(err);
+            }
+            else {
+                let response = {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        bucket: data[0].Bucket,
+                        key: data[0].Key,
+                        message: data[1]
+                    })
+                };
+                callback(undefined, response);
+            }
+        });
+    }
+};
 
 module.exports.upload = (event, context, callback) => {
-    let bucket = process.env['BUCKET_NAME'];
-    RequestHandler.perform(event, context, callback, new FileBuilder(), new FileWriter(new AWSS3()),bucket );
+    let imageUploader = new ImageUploader();
+    imageUploader.perform(event, context, callback);
 };
