5375b73f45568b3710e28484ce533fd0c212fb63
stretch1414 <jmoser1414@gmail.com>
2018-03-27 01:58:28 -0400

    Modify test for asynchronous code

--- lambda/test/base64-handler.js
+++ lambda/test/base64-handler.js
@@ -5,8 +5,16 @@ let Base64Handler = require('../base64-handler');
 let base64Image = 'iVBORw0KGgoAAAANSUhEUgAAAAsAAAAECAYAAABY+sXzAAAABHNCSVQICAgIfAhkiAAAAFlJREFUCJl9yjEKwCAUBNERv43g/U+WKqVVsNRKxU0jJGnyYGGLcTln1VoBSCnRWsN7D0AIATOjlEKMEbQd16kxhuac6r1rrfWZJBmbAEn8eeKlT/z+zjkAbkDFRMbggmGwAAAAAElFTkSuQmCC'
 
 describe("Base64 handler", function () {
-    it("Gets the correct mime type", function () {
-        expect(Base64Handler.getMimeType(Base64Handler.getBuffer(base64Image))).to.deep.include({ ext: 'png', mime: 'image/png' } )
+    it("Gets the correct mime type", function (done) {
+        Base64Handler.getMimeType(Base64Handler.getBuffer(base64Image), (err, mimeType) => {
+        	if (err) {
+        		done(err);
+        	}
+        	else {
+        		expect(mimeType).to.deep.include({ ext: 'png', mime: 'image/png' } )
+        		done();
+        	}
+        });
     });
 
     it("Removes everything in front of comma, inclusive", function () {
