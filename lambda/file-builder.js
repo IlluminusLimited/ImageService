74e360ddc16f8a3c5918bb49f63243788077c333
stretch1414 <jmoser1414@gmail.com>
2018-04-01 02:34:10 -0400

    Remove unnecessary parameter

--- lambda/file-builder.js
+++ lambda/file-builder.js
@@ -2,7 +2,7 @@ const Base64Handler = require("./base64-handler");
 const md5 = require('md5');
 
 module.exports = class FileBuilder {
-    getFile(image, callback) {
+    getFile(image) {
         let buffer = Base64Handler.getBuffer(Base64Handler.pruneBase64String(image));
         let mimeType = Base64Handler.getMimeType(image);
         let fileName = 'raw/' + md5(buffer) + '.' + mimeType.subtype;
