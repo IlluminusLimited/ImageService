45c8722791ff78828134e87273f8f81abd8b65ec
scytherswings <scytherswings@gmail.com>
2018-04-22 14:57:57 -0400

    Make error handling more informative

--- lambda/image-manipulator.js
+++ lambda/image-manipulator.js
@@ -18,17 +18,32 @@ if (process.env.ALLOWED_DIMENSIONS) {
 
 exports.generateThumbnail = function generateThumbnail(event, context, callback) {
     const key = event.queryStringParameters.key;
-    const match = key.match(/((\d+|auto)x(\d+|auto))(.*)/);
-    const dimensions = match[1];
-    const width = match[2] === 'auto' ? null : Math.min(parseInt(match[2], 10), MAX_SIZE);
-    const height = match[3] === 'auto' ? null : Math.min(parseInt(match[3], 10), MAX_SIZE);
-    const originalKey = match[4];
+    const match = key.match(/(([\/a-zA-Z0-9]+)\/([a-zA-Z0-9]+))_((\d+|auto)x(\d+|auto))(\.jpeg|\.jpg|\.png)/);
+
+    if (match === null) {
+        callback(null, {
+            statusCode: '400',
+            headers: {},
+            body: "Key: '" + key + "' is not a supported image file",
+        });
+        return;
+    }
+
+    const dimensions = match[4];
+    const width = match[5] === 'auto' ? null : Math.min(parseInt(match[5], 10), MAX_SIZE);
+    const height = match[6] === 'auto' ? null : Math.min(parseInt(match[6], 10), MAX_SIZE);
+    const prefix = match[2];
+    const dotExtension = match[7];
+    const originalKey = match[1] + dotExtension;
+    const newKey = match[1] + ".jpg";
+
 
     console.log("Dimensions " + dimensions);
     console.log("Width " + width);
     console.log("Height " + height);
+    console.log("Prefix " + prefix);
+    console.log("DotExtension " + dotExtension);
     console.log("OriginalKey " + originalKey);
-
 
     if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
         callback(null, {
@@ -39,7 +54,7 @@ exports.generateThumbnail = function generateThumbnail(event, context, callback)
         return;
     }
 
-    S3.getObject({ Bucket: BUCKET, Key: originalKey }).promise()
+    S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     // eslint-disable-next-line new-cap
         .then(data => Sharp(data.Body)
             .resize(width, height)
@@ -50,14 +65,14 @@ exports.generateThumbnail = function generateThumbnail(event, context, callback)
                 Body: buffer,
                 Bucket: BUCKET,
                 ContentType: 'image/jpeg',
-                Key: key,
+                Key: newKey,
                 CacheControl: `max-age=${MAX_AGE}`,
             }).promise()
         )
         .then(() => callback(null, {
                 statusCode: '301',
                 headers: {
-                    location: `${URL}/${key}`,
+                    location: `${URL}/${newKey}`,
                     'Cache-Control': 'no-cache, no-store, must-revalidate',
                     Pragma: 'no-cache',
                     Expires: '0',
