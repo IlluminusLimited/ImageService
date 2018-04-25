a549b96ce0b752c71647c95e08d5567f28b0b61b
scytherswings <scytherswings@gmail.com>
2018-04-25 00:30:14 -0400

    bed time comments

--- lambda/lib/image-transformer.js
+++ lambda/lib/image-transformer.js
@@ -1,5 +1,6 @@
 const Sharp = require('sharp');
 
+// Make this not static, should be like a no args constructor or something.
 module.exports = class ImageTransformer {
    static transformImage(body, width, height, format = 'jpeg') {
         return Sharp(body)
