b5cfad86049992659f6fae76da1ee8bf63632a66
scytherswings <scytherswings@gmail.com>
2018-05-19 21:36:21 -0400

    add serverless secret stuff

--- lambda/lib/pinster-api-client.js
+++ lambda/lib/pinster-api-client.js
@@ -3,37 +3,48 @@
 const _ = require('lodash');
 const util = require('util');
 const request = require('request');
+const secretsPromise = require('serverless-secrets/client').load();
 
 module.exports = class PinsterApiClient {
-    constructor(base_url, auth_token) {
-        this.base_url = _.isUndefined(base_url) ? process.env.PINSTER_API_URL : base_url;
-        this.auth_token = _.isUndefined(auth_token) ? process.env.AUTH_TOKEN : auth_token;
+    constructor(baseUrl, authToken) {
+        secretsPromise.then(() => {
+            this.baseUrl = _.isUndefined(baseUrl) ? process.env.PINSTER_API_URL : baseUrl;
+
+            this.authToken = _.isUndefined(authToken) ? process.env.AUTH_TOKEN : authToken;
+        }).catch(err => {
+            console.log("Failed to retrieve secrets!");
+            console.error(err);
+        });
     }
 
     createImage(imageParameters, callback) {
-        request.post({
-            url: `${this.base_url}/v1/images`,
-            headers: {
-                'Authorization': this.auth_token
-            }
-        }, imageParameters, (err, response, body) => {
-            if (err) {
-                console.log(util.inspect(err, {depth: 5}));
-                callback(err);
-            }
-            else {
-                console.log(util.inspect(response, {depth: 5}));
-                console.log(util.inspect(body, {depth: 5}));
-                callback(undefined, response, body);
-            }
-        });
+        if (_.isUndefined(this.authToken)) {
+            callback('Secret fetching failed!');
+        } else {
+            request.post({
+                url: `${this.baseUrl}/v1/images`,
+                headers: {
+                    'Authorization': this.authToken
+                }
+            }, imageParameters, (err, response, body) => {
+                if (err) {
+                    console.log(util.inspect(err, {depth: 5}));
+                    callback(err);
+                }
+                else {
+                    console.log(util.inspect(response, {depth: 5}));
+                    console.log(util.inspect(body, {depth: 5}));
+                    callback(undefined, response, body);
+                }
+            });
+        }
     }
 
     createFailureNotification(notificationParameters, callback) {
         request.post({
-            url: `${this.base_url}/v1/notifications/failure`,
+            url: `${this.baseUrl}/v1/notifications/failure`,
             headers: {
-                'Authorization': this.auth_token
+                'Authorization': this.authToken
             }
         }, notificationParameters, (err, response, body) => {
             if (err) {
