20a9f831130ffe55206d2a216a350b2e283da96b
scytherswings <scytherswings@gmail.com>
2018-05-22 23:47:52 -0400

    cors fuckery

--- lambda/lib/http-response-builder.js
+++ lambda/lib/http-response-builder.js
@@ -4,7 +4,10 @@ module.exports = class HttpResponseBuilder {
     constructor(statusCode, body, headers = {}) {
         this.statusCode = statusCode;
         this.body = body;
-        this.headers = headers;
+        this.headers = {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+        };
     }
 
     generateResponse() {
